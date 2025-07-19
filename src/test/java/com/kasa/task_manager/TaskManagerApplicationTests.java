d912ed01cbcb5b87a2e4b99a47d91386a9909145
shaurya <emorres25@gmail.com>
2025-07-19 13:24:02 +0530

    adding test resources

--- src/test/java/com/kasa/task_manager/TaskManagerApplicationTests.java
+++ src/test/java/com/kasa/task_manager/TaskManagerApplicationTests.java
@@ -3,7 +3,7 @@ package com.kasa.task_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TestConfig.class)
+@SpringBootTest
 class TaskManagerApplicationTests {
 
 	@Test
