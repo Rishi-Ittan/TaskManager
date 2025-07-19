c6fb22d9eae1dd56ae389e9b6f0724891af017c0
shaurya <emorres25@gmail.com>
2025-07-19 13:25:41 +0530

    removing test

--- src/test/java/com/kasa/task_manager/TaskManagerApplicationTests.java
+++ src/test/java/com/kasa/task_manager/TaskManagerApplicationTests.java
@@ -3,7 +3,6 @@ package com.kasa.task_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TaskManagerApplicationTests {
 
 	@Test
